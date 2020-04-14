ed99e01173e705f296f06af4bb94183f1a99d592
suhaspotluri <54907667+suhaspotluri@users.noreply.github.com>
2020-04-14 16:29:24 -0400

    Attempting EC

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const url = "https://cfw-takehome.developers.workers.dev/api/variants";
 const fetch = require('node-fetch');
 const random= require('random');
 
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
+new HTMLRewriter.on('title',new ElementHandler()).onDocument( new DocumentHandler())
+
 async function handleRequest(request) {
 	let response = await fetch(url);
 	let json = await response.json();
@@ -22,3 +22,41 @@ async function handleRequest(request) {
     headers: { 'content-type': 'text/html' },
   })
 }
+
+class ElementHandler {
+  element(element) {
+    // An incoming element, such as `div`
+    console.log(`Incoming element: ${element.tagName}`)
+  }
+
+  comments(comment) {
+    // An incoming comment
+  }
+
+  text(text) {
+    // An incoming piece of text
+  }
+}
+
+
+
+async function handleRequest(req) {
+  const res = await fetch(req)
+
+  return new HTMLRewriter().on('div', new ElementHandler()).transform(res)
+}
+
+class DocumentHandler {
+  doctype(doctype) {
+    // An incoming doctype, such as <!DOCTYPE html>
+  }
+
+  comments(comment) {
+    // An incoming comment
+  }
+
+  text(text) {
+    // An incoming piece of text
+  }
+}
+	
