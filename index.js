351fd1ae4fb0174dbaa65e96fb8e633d6a8de9ac
suhaspotluri <54907667+suhaspotluri@users.noreply.github.com>
2020-04-14 22:48:30 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,60 +1,78 @@
-
+//setting api url and required modules
 const url = "https://cfw-takehome.developers.workers.dev/api/variants";
 const fetch = require('node-fetch');
 const random= require('random');
 
+//event listener
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+//class to replace anchor link with my own
 class AttributeRewriter {
   constructor(attributeName) {
+	  //which attribute of a tag to alter
     this.attributeName = attributeName
   }
  
   element(element) {
+	  //getting element with href attribute
     const attribute = element.getAttribute(this.attributeName)
     if (attribute) {
       element.setAttribute(
         this.attributeName,
+		//replacing href with new url
         attribute.replace('https://cloudflare.com', 'https://suhasthebest.pythonanywhere.com/hangman')
       )
+	  //changing link text
 	  element.setInnerContent("Visit My Latest Project");
 	  
     }
   }
 }
 
+//class to replace text of different elements
 class InnerRewriter {
 	constructor(adText,prep) {
+	//text to replace
     this.adText = adText;
+	//should text be prepended or replaced 
 	this.prep = prep;
   }
   element(element) {
 	  if(this.prep){
+		  //prepend text
 		  element.prepend(this.adText);
 	  }
+		// replace text
 	  else{
 		  element.setInnerContent(this.adText);
 	  }
     }
   }
-  
+ //creating rewriter objects of above classes 
 var title = new InnerRewriter("Suhas Potluri",false);
 var h1 =new InnerRewriter("This is Site ",true);
 var p= new InnerRewriter("Cloudflare Fullstack Internship Coding Challenge",false);
 var a= new AttributeRewriter('href');
+//using HTML Rewriter class and passing above rewriter objects
 const rewriter = new HTMLRewriter().on('title', title).on('p#description',p).on('h1#title',h1).on('a',a);
   
-  
+  //async function, sending response
   async function handleRequest(request) {
+	//gettin api url response 
 	let response = await fetch(url);
+	//parsing in json
 	let json = await response.json();
+	//generating random int 0-1
 	let rand = random.int(min=0,max=1);
+	//selecting random index of variants array
 	let siteresponse = await fetch(json['variants'][rand]);
+	//loading response from site
 	let siteRes= await siteresponse;
+	//calling HTMLRewriter
 	let transRes= await rewriter.transform(siteRes).text()
-	
+	//returning the response for render
 	return new Response(transRes, {headers: { 'content-type': 'text/html' },})
 }
 
